d42d8d9ebb45d608f741c99205a0046a500d9885
davidegarr <davidea97@gmail.com>
2023-09-22 22:42:33 +0100

    now calculates and adds csp3 quota from raw to clean data

--- cleaner.py
+++ cleaner.py
@@ -1,25 +1,66 @@
 import csv
+from rdkit import Chem
 
-
-#opens source file and creates the reader
-with open("raw_data.csv", "r") as raw_data:
-    csvreader = csv.DictReader(raw_data, delimiter = ";")
-    
-    #opens target file and creates the writer
-    with open("clean_data.csv", "w", newline='') as clean_data:
-        headers = ["ChEMBL_ID", "SMILES", "MolecularWeight", "MaxClinicalPhase"]
-        csvwriter = csv.DictWriter(clean_data, fieldnames=headers)
+def main():
+    #opens source file and creates the reader
+    with open("raw_data.csv", "r") as raw_data:
+        csvreader = csv.DictReader(raw_data, delimiter = ";")
         
-        csvwriter.writeheader()
-        #copies only the indicated values in the new row if all of them are present in source
-        for row in csvreader:
-            if any(value == "" for value in row.values()):
-                continue
-            csvwriter.writerow({
-                'ChEMBL_ID': str(row['ChEMBL ID']), 
-                'SMILES': str(row['Smiles']), 
-                'MolecularWeight': float(row['Molecular Weight']), 
-                'MaxClinicalPhase': float(row['Max Phase'])
-            })
+        #opens target file and creates the writer
+        with open("clean_data.csv", "w", newline='') as clean_data:
+            headers = ["ChEMBL_ID", "SMILES", "MolecularWeight", "MaxClinicalPhase", "csp3_quota"]
+            csvwriter = csv.DictWriter(clean_data, fieldnames=headers)
+            
+            csvwriter.writeheader()
+            #copies only the indicated values in the new row if all of them are present in source
+            for row in csvreader:
+                if any(value == "" for value in row.values()):
+                    continue
+                
+                csp3_quota = get_csp3_quota(str(row["Smiles"]))
+                if csp3_quota == None:
+                    continue
+
+                csvwriter.writerow({
+                    'ChEMBL_ID': str(row['ChEMBL ID']), 
+                    'SMILES': str(row['Smiles']), 
+                    'MolecularWeight': float(row['Molecular Weight']), 
+                    'MaxClinicalPhase': float(row['Max Phase']),
+                    "csp3_quota": csp3_quota
+                })
+
+def get_csp3_quota(smiles):
+    print(smiles)
+    molecule = Chem.MolFromSmiles(smiles)
+    num_atoms = molecule.GetNumAtoms()
+    sp3_carbons = 0
+    total_carbons = 0
+
+    for atom_idx in range(num_atoms):
+        atom = molecule.GetAtomWithIdx(atom_idx)
+        symbol = atom.GetSymbol()
 
 
+        if symbol != "C":
+            continue
+
+        total_carbons += 1
+        hybridization = get_hybridization(molecule, atom_idx)
+        
+        if hybridization == Chem.rdchem.HybridizationType.SP3:
+            sp3_carbons += 1
+    try:
+        quota = float(sp3_carbons/total_carbons)
+        return round(quota, 3)
+    except ZeroDivisionError:
+        return None
+    
+
+
+
+def get_hybridization(molecule, atom_idx):
+    atom = molecule.GetAtomWithIdx(atom_idx)  # Get atom by index
+    hybridization = atom.GetHybridization()  # Retrieve hybridization
+    return hybridization
+
+main()
